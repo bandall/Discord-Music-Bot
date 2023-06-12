b62dee61236d9ae9f82cdc4b0bd7c0d8b868cecb
bandall <jsm5315@naver.com>
2023-06-12 09:23:53 +0900

    add local song metadata

--- src/api-controller.js
+++ src/api-controller.js
@@ -1,14 +1,17 @@
 import axios from "axios";
+import * as https from "https";
 import { log_server } from "./util";
 
 const SERVER_URL = "https://bandallgom.com:8000/api/"
-
+const httpsAgent = new https.Agent({
+    rejectUnauthorized: false
+})
 export const getFileInfo = async (fileId) => {
     let res;
     try {
         axios.defaults.withCredentials = true;
         const url = SERVER_URL + "file-info/" + fileId;
-        res = await axios.get(url);
+        res = await axios.get(url, { httpsAgent });
         if (res.data.hasFile) {
             return res.data.fileMetadata.originalFileName;
         } else {
